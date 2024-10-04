e74db8512b32cab0fb97eea125ddff7e927ea414
Samuele Castaldo <castaldo.samuele@gmail.com>
2024-10-04 11:02:22 +0200

    creazione di una lista prova, per testare l'aggiunta del gioco

--- src/main/java/samueleCastaldo/Application.java
+++ src/main/java/samueleCastaldo/Application.java
@@ -1,9 +1,29 @@
 package samueleCastaldo;
 
+import samueleCastaldo.giochi.Gioco;
+import samueleCastaldo.giochi.GiocoDaTavolo;
+import samueleCastaldo.giochi.Videogioco;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class Application {
 
     public static void main(String[] args) {
+        Collezione collezione = new Collezione(); //creo l'oggetto collezione, (andrà a creare la lista vuota giochi)
 
-        System.out.println("Hello World!");
+        List<Gioco> listaProva = Arrays.asList(
+                new Videogioco(1, "The Witcher 3", "2015", 49.99, "PC", 50, Videogioco.Genere.AVVENTURA),
+                new GiocoDaTavolo(2, "Monopoly", "1935", 29.99, 4, 120),
+                new Videogioco(3, "FIFA 21", "2020", 59.99, "PS5", 30, Videogioco.Genere.SPORT),
+                new GiocoDaTavolo(4, "Catan", "1995", 39.99, 4, 90),
+                new Videogioco(5, "Dark Souls", "2011", 39.99, "PC", 40, Videogioco.Genere.AZIONE),
+                new GiocoDaTavolo(6, "Ticket to Ride", "2004", 34.99, 2, 30)
+        );
+
+
+
+
+
     }
 }
