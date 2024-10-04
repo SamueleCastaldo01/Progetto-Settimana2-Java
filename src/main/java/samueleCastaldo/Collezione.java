bee279f1ee61a03c2c50c1f91a8e5193d6b3f91e
Samuele Castaldo <castaldo.samuele@gmail.com>
2024-10-04 10:54:59 +0200

    creazione della classe Collezione

---
+++ src/main/java/samueleCastaldo/Collezione.java
@@ -0,0 +1,19 @@
+package samueleCastaldo;
+
+import samueleCastaldo.giochi.Gioco;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Collezione {
+    private List<Gioco> giochi;
+
+    public Collezione() {
+        this.giochi = new ArrayList<>(); //in questo modo andiamo a creare una lista vuota
+    }
+
+
+
+
+
+}
