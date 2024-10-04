5174e45f2b8f5c135ba02f5b8c323e033c48b81e
Samuele Castaldo <castaldo.samuele@gmail.com>
2024-10-04 10:32:08 +0200

    creazione della classe Videogioco

---
+++ src/main/java/samueleCastaldo/giochi/Videogioco.java
@@ -0,0 +1,53 @@
+package samueleCastaldo.giochi;
+
+public class Videogioco  extends Gioco{
+    private String piattaforma;
+    private int durataDiGioco;
+
+    public enum Genere {
+        AZIONE,
+        AVVENTURA,
+        GDR,
+        SPARATUTTO,
+        STRATEGIA,
+        SPORT,
+        CORSE,
+        SIMULAZIONE,
+        HORROR,
+        PUZZLE,
+        PLATFORM
+    }
+    private Genere genere;
+
+
+    public Videogioco(long idGioco, String titolo, String annoDiProduzione, double prezzo, String piattaforma, int durataDiGioco, Genere genere) {
+        super(idGioco, titolo, annoDiProduzione, prezzo);
+        this.piattaforma = piattaforma;
+        this.durataDiGioco = durataDiGioco;
+        this.genere = genere;
+    }
+
+    public String getPiattaforma() {
+        return piattaforma;
+    }
+
+    public int getDurataDiGioco() {
+        return durataDiGioco;
+    }
+
+    public Genere getGenere() {
+        return genere;
+    }
+
+    public void setPiattaforma(String piattaforma) {
+        this.piattaforma = piattaforma;
+    }
+
+    public void setDurataDiGioco(int durataDiGioco) {
+        this.durataDiGioco = durataDiGioco;
+    }
+
+    public void setGenere(Genere genere) {
+        this.genere = genere;
+    }
+}
